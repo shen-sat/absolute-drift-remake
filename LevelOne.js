109405acaeddd51a6c449a513045a61a50ee5f7c
Shen Sat <shensat@shens-mbp.home>
2019-11-19 22:34:53 +0000

    Simple straight line drift

--- LevelOne.js
+++ LevelOne.js
@@ -13,14 +13,16 @@ class LevelOne extends Phaser.Scene {
 	create() {
 
 		this.car = this.physics.add.image(120, 448.0, 'car');
+		this.ghost = this.physics.add.image(120, 448.0, 'car');
+		this.ghost.alpha = 0;
 		this.keys = this.input.keyboard.createCursorKeys();
 
 		this.speed = 0;
 		this.acceleration = 2;
-		this.drag = 3;
+		this.drag = 2;
 		this.speedUpperLimit = 200;
-		this.previousAngle = this.car.angle;
 		this.turn = 90;
+		this.friction = 0;
 	}
 
 	update() {
@@ -30,18 +32,22 @@ class LevelOne extends Phaser.Scene {
 			this.speed = Math.max(0, this.speed -= this.drag);
 		}
 
-		//Turning
-		if (this.speed != 0) {
-			if (this.keys.right.isDown) {
-				this.speed > 0 ? this.car.angle += 2 : this.car.angle -= 2;
-				this.speed > 150 ? this.turn -= 1 : this.turn = 90; 
-			} else if (this.keys.left.isDown) {
-				this.speed > 0 ? this.car.angle -= 2 : this.car.angle += 2;
-			}	
+		if (this.keys.right.isDown) {
+			this.car.angle += 1;
+			// console.log(1-((this.car.angle - this.ghost.angle)/360)*2);
 		}
-
+		this.friction = 1-(((this.car.angle - this.ghost.angle)/10000)*2);
+		this.speed = this.speed * this.friction;
 		
-		this.physics.velocityFromAngle(this.car.angle + this.turn, -this.speed, this.car.body.velocity);	
+
+
+
+
+
+
+		this.physics.velocityFromAngle(this.ghost.angle + 90, -this.speed, this.ghost.body.velocity);
+		this.car.x = this.ghost.x;
+		this.car.y = this.ghost.y;	
 
 		
 	}
